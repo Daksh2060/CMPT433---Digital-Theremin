615c4cbe758f0e0718ed1732d1b2299db5549aa5
Daksh2060 <daksh21697@gmail.com>
2025-03-29 19:07:17 -0700

    Added backup sensor method

--- hal/include/distance_sensor.h
+++ hal/include/distance_sensor.h
@@ -10,5 +10,22 @@ int get_distance();
 
 void distance_sensor_cleanup();
 
-
 #endif
+
+
+// Backup
+
+// #ifndef _DISTANCE_SENSOR_H_
+// #define _DISTANCE_SENSOR_H_
+
+// #include <stdint.h>
+// #include <stdbool.h>
+
+
+// int distance_sensor_init(void);
+
+// void distance_sensor_cleanup(void);
+
+// double get_distance(void);
+
+// #endif
--- hal/src/distance_sensor.c
+++ hal/src/distance_sensor.c
@@ -127,3 +127,118 @@ static long double get_distance_cm()
     }
     return distance_in_cm;
 }
+
+
+//# BACKUP
+
+// #include "distance_sensor.h"
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <stdbool.h>
+// #include <time.h>
+// #include <limits.h>
+
+// #define ECHO_GPIO_PIN "4"
+// #define TRIGGER_GPIO_PIN "6"
+// #define HIGH '1'
+// #define LOW '0'
+
+// #define SPEED_OF_SOUND 343.0
+// #define MAX_TIME 13000000
+// #define UNIT_SCALE 10000000.0 
+
+// static int trigger_fd = -1;
+// static int echo_fd = -1;
+
+// static uint64_t get_time_ns(void) {
+//     struct timespec ts;
+//     clock_gettime(CLOCK_MONOTONIC, &ts);
+//     return (uint64_t)ts.tv_sec * 1e9 + ts.tv_nsec;
+// }
+
+
+// int distancedistance_sensor_init_init(void) {
+
+//     char trigger_path[64];
+//     snprintf(trigger_path, sizeof(trigger_path), "/sys/class/gpio/gpio%s/value", TRIGGER_GPIO_PIN);
+//     trigger_fd = open(trigger_path, O_RDWR);
+//     if (trigger_fd == -1) {
+//         perror("Failed to open trigger");
+//         return -1;
+//     }
+
+//     char echo_path[64];
+//     snprintf(echo_path, sizeof(echo_path), "/sys/class/gpio/gpio%s/value", ECHO_GPIO_PIN);
+//     echo_fd = open(echo_path, O_RDONLY);
+//     if (echo_fd == -1) {
+//         perror("Failed to open echo");
+//         close(trigger_fd);
+//         return -1;
+//     }
+
+//     return 0;
+// }
+
+// void distance_sensor_cleanup(void) {
+//     if (trigger_fd != -1) {
+//         close(trigger_fd);
+//         trigger_fd = -1;
+//     }
+
+//     if (echo_fd != -1) {
+//         close(echo_fd);
+//         echo_fd = -1;
+//     }
+// }
+
+// static bool read_echo(void) {
+//     char echo_value;
+//     if (read(echo_fd, &echo_value, 1) == -1) {
+//         perror("Failed to read echo file");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     lseek(echo_fd, 0, SEEK_SET);
+
+//     return echo_value == HIGH;
+// }
+
+
+// double get_distance(void) {
+
+//     char high_value = HIGH;
+//     if (write(trigger_fd, &high_value, 1) == -1) {
+//         perror("Failed to write HIGH to trigger");
+//         exit(EXIT_FAILURE);
+//     }
+//     usleep(10);
+
+//     char low_value = LOW;
+//     if (write(trigger_fd, &low_value, 1) == -1) {
+//         perror("Failed to write LOW to trigger");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     uint64_t real_start = get_time_ns();
+//     uint64_t start = real_start, end = real_start;
+
+//     while (!read_echo()) {
+//         start = get_time_ns();
+//         if (start - real_start > MAX_TIME) {
+//             return -1;
+//         }
+//     }
+
+//     while (read_echo()) {
+//         end = get_time_ns();
+//         if (end - real_start > MAX_TIME) {
+//             return -1;
+//         }
+//     }
+
+//     uint64_t diff = end - start;
+//     return (diff * SPEED_OF_SOUND) / UNIT_SCALE / 2.0;
+// }

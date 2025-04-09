66141953479d8041d154d5cc1a11e71e8f7aae61
roland <rrp2001@gmail.com>
2025-04-09 11:18:28 -0700

    added to the python udp send script some lines for easy laptop-to-board mediapipe processing

--- python/udp_module.py
+++ python/udp_module.py
@@ -1,7 +1,9 @@
 import socket
 
 def send_data(data):
-    UDP_IP = "127.0.0.1"
+    LOCALHOST = "127.0.0.1"
+    LAPTOP_TO_BOARD = "192.168.7.2"
+    UDP_IP = LOCALHOST
     UDP_PORT = 12345
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

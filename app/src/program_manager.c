afe2a5aa9bd433d133b2921e653c024ae0f3e2a9
roland <rrp2001@gmail.com>
2025-04-08 18:39:05 -0700

    added audio module init and cleanup to the program manager.

--- app/src/program_manager.c
+++ app/src/program_manager.c
@@ -1,23 +1,26 @@
 
+#include "program_manager.h"
 #include "button_controls.h"
 #include "hand_commands.h"
 #include "dial_controls.h"
 #include "udp_controls.h"
 #include "utils.h"
-#include "program_manager.h"
 #include "sine_mixer.h"
 #include "gpio.h"
 #include "lcd_menus.h"
-#include <stdio.h>
 #include "distance_sensor.h"
+#include "distance_articulator.h"
+#include <stdio.h>
 
 volatile bool exit_theremin_program = false;
 
 void program_manager_init(void)
 {
+    SineMixer_init();
     lcd_menu_init();
     udp_init();
     distance_sensor_init();
+    distance_articulator_init();
     command_handler_init();
     dial_controls_init();
     button_controls_init();
@@ -36,7 +39,9 @@ void program_manager_cleanup(void)
     dial_controls_cleanup();
     gpio_cleanup();
     udp_cleanup();
+    distance_articulator_cleanup();
     distance_sensor_cleanup();
     command_handler_cleanup();
     lcd_menu_cleanup();
+    SineMixer_cleanup();
 }

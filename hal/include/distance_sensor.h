f13621ece662a68e71c69db2a4cd714ae11ef4bf
Daksh2060 <daksh21697@gmail.com>
2025-04-09 18:07:07 -0700

    Comments for the HAL header files

--- hal/include/distance_sensor.h
+++ hal/include/distance_sensor.h
@@ -1,13 +1,35 @@
+/*
+ * This module is used to interface with a Distance Sensor.
+ * It allows the creation of a pthread which actively reads
+ * and adjusts the value of the distance sensor based on the location
+ * of the directly object in front of it.
+ */
+
+
 #ifndef _DISTANCE_SENSOR_H_
 #define _DISTANCE_SENSOR_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+
+/*
+ * This function initializes the distance sensor module and starts the read thread.
+ */
 void distance_sensor_init();
 
+
+/**
+ * This function retrieves the current distance value from the sensor.
+ * 
+ * @return The current distance in cm.
+ */
 int get_distance();
 
+
+/*
+ * This function cleans up the distance sensor module and stops the read thread.
+ */
 void distance_sensor_cleanup();
 
 #endif
--- hal/include/sine_mixer.h
+++ hal/include/sine_mixer.h
@@ -1,10 +1,18 @@
-// allows for the playback of a single continuous frequency.
+/* 
+ * This module is used to interface with a Sine Mixer. It constantly plays
+ * a sine wave at a given frequency and allows the user to change the
+ * frequency, waveform, and volume of the sound. It also allows for
+ * distortion of the frequency to simulate different sound effects.
+ */
+
+
 #ifndef _SINE_MIXER_H
 #define _SINE_MIXER_H
 
 #define SINEMIXER_VOLUME_MAX 100
 #define SINEMIXER_VOLUME_MIN 0
 
+// Different waveforms that can be played
 enum SineMixer_waveform{
     SINEMIXER_WAVE_SINE,
     SINEMIXER_WAVE_SQUARE,
@@ -16,39 +24,82 @@ enum SineMixer_waveform{
     SINEMIXER_WAVE_COUNT
 };
 
+
+/*
+ * Initializes the SineMixer module. Must be called before any other functions.
+ * This function sets up the audio playback system and prepares it for use.
+ */
 void SineMixer_init(void);
-void SineMixer_cleanup(void);
+
 
 /**
- * @brief queues the continous playback of the given frequency. 
+ * Queues the continous playback of the given frequency. 
  * Replaces the currently playing frequency if there is one.
  * @param frequency the frequency of the sine wave to be played
  */
 void SineMixer_queueFrequency(double frequency);
-double SineMixer_getFrequency(void);
+
 
 /**
- * @brief stops the playback of the current frequency, if there is one.
+ * Gets the currently playing frequency.
+ * @return the currently playing frequency.
+ */
+double SineMixer_getFrequency(void);
+
+
+/*
+ * Stops the playback of the current frequency, if there is one.
  */
 void SineMixer_stopPlayback(void);
 
+
 /**
- * @brief changes the currently playing waveform to the desired one.
+ * Changes the currently playing waveform to the desired one.
  * @param waveform the waveform to change to.
  */
 void SineMixer_setWaveform(enum SineMixer_waveform waveform);
-enum SineMixer_waveform Sinemixer_getWaveform(void);
+
 
 /**
- * @brief adds a random amount of offset to the playing frequency.
+ * Gets the currently playing waveform.
+ * @return the currently playing waveform.
+ */
+enum SineMixer_waveform Sinemixer_getWaveform(void);
+
+
+/**
+ * Adds a random amount of offset to the playing frequency.
  * the amount of offset is relative to the given frequency
  * @param distortion the % amount of distortion to add to a given note.
  * for example, 0.01 adds 1% distortion
  */
 void SineMixer_setDistortion(double distortion);
+
+
+/**
+ * Gets the current distortion value.
+ * @return the current distortion value.
+ */
 double SineMixer_getDistortion(void);
 
+
+/**
+ * Get the volume of the audio playback.
+ * @return The current volume level (0-100).
+ */
 int SineMixer_getVolume(void);
+
+
+/**
+ * Sets the volume of the audio playback.
+ * @param newVolume the new volume level (0-100).
+ */
 void SineMixer_setVolume(int newVolume);
+
+
+/*
+ * Cleans up the SineMixer resources and stops playback.
+ */
+void SineMixer_cleanup(void);
 
 #endif
